95bb536443ba30aa9b1387cbc4ad9b10cfae9d6a
Ankur Mundra <amundra@redhat.com>
2022-06-15 12:56:07 -0400

    function to add on metadata cleaning, removes status and nodeName

---
+++ clean-resource/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+)
+
+// Run
+// This is the main logic. rl is the input `ResourceList` which has the `FunctionConfig` and `Items` fields.
+// You can modify the `Items` and add result information to `rl.Result`.
+func Run(rl *fn.ResourceList) (bool, error) {
+	for _, kubeObject := range rl.Items {
+		if err := cleanResourceManifest(kubeObject); err != nil {
+			return false, err
+		}
+	}
+
+	//	This result message will be displayed in the function evaluation time.
+	rl.Results = append(rl.Results, fn.GeneralResult("Remove unnecessary fields from resource", fn.Info))
+	return true, nil
+}
+
+func main() {
+	if err := fn.AsMain(fn.ResourceListProcessorFunc(Run)); err != nil {
+		os.Exit(1)
+	}
+}
